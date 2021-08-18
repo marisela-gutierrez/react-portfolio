818c0da55d3d17f42c08c92bb7cbdd609ad13e15
marigut <mari.gutierrez96@yahoo.com>
2021-08-18 01:17:49 -0500

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://github.com/marisela-gutierrez" target="_blank" rel="noreferrer">Github</a>
+        </li>
+        <li>
+          <a href="www.linkedin.com/in/mariselagutierrez" target="_blank" rel="noreferrer">LinkedIn</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
