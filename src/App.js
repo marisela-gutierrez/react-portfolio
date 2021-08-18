e600552d1e2775df550183f14524a6331b92c6ae
marigut <mari.gutierrez96@yahoo.com>
2021-08-18 00:43:10 -0500

    contact-me

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [sections] = useState([
     { name: "About Me" },
     { name: "Portfolio" },
+    { name: "Contact Me" },
   ]);
 
   const [currentSection, setCurrentSection] = useState(sections[0]);
@@ -22,6 +24,7 @@ function App() {
       <main>
         {currentSection.name === "About Me" && <About />}
         {currentSection.name === "Portfolio" && <Portfolio />}
+        {currentSection.name === "Contact Me" && <ContactForm />}
       </main>
     </div>
   );
